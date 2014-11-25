5ab4608352aaafaf0ddf322fce110171f2c549df
Giorgix <jackosinapsis@gmail.com>
2014-11-25 18:24:43 +0100

    added priority filters

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,27 +1,55 @@
 var app = angular.module('todoApp', []);
 
+app.filter('priority', function() {
+  return function(items, priorityOpt) {
+    //console.log(items);
+    if(priorityOpt == 'all') {
+      return items;
+    }
+    filteredItems = [];
+    for(var i=0; i < items.length;i++) {
+      console.log(items[i]);
+      if(items[i].priority == priorityOpt){
+        filteredItems.push(items[i]); 
+      } else {
+        continue;
+      }
+    }
+    return filteredItems;
+  }
+});
 
 app.controller('TodoCtrl', function($scope) {
+  $scope.priorityOpts = ['all', 'high', 'normal', 'low'];
   $scope.todos = [
     {
       content: 'first todo',
-      completed: false
+      completed: false,
+      priority: 'normal'
       
     },
     {
       content: 'second todo',
-      completed: false
+      completed: false,
+      priority: 'high'
     },
     {
       content: 'third todo',
-      completed: false
+      completed: false,
+      priority: 'low'
     }
   ]
   $scope.addTodo = function() {
     var newTodo = {
-      content: $scope.newTodo.trim()
+      content: $scope.newTodo.trim(),
+      completed : false,
+      priority: $scope.priorityOpt
+    };
+    if(!newTodo.content) {
+      return;
     }
     $scope.todos.push(newTodo);
+    $scope.newTodo = '';
   }
   
   $scope.removeTodo = function(todo) {
