b0d08e052f20df5e79434876ba5db97db1402d17
Giorgix <jackosinapsis@gmail.com>
2014-12-17 14:26:42 +0100

    added new todo e2e test

--- test/e2e/spec.js
+++ test/e2e/spec.js
@@ -1,7 +1,16 @@
 describe('todoapp homepage', function() {
+  var todos = element.all(by.repeater('todo in todos'));
+  
   it('should have a title', function() {
     browser.get('http://localhost:3000');
 
     expect(browser.getTitle()).toEqual('TodoApp');
   });
+
+  it('should add new todo', function() {
+    browser.get('http://localhost:3000');
+    element(by.model('newTodo')).sendKeys('todo test e2e');
+    element(by.id('addButton')).click();
+    expect(todos.count()).toEqual(3);
+  })
 });
