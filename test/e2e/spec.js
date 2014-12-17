c4588c3c72704588219d5602f106338681fdeb2c
Giorgix <jackosinapsis@gmail.com>
2014-12-17 15:04:58 +0100

    added delete e2e test

--- test/e2e/spec.js
+++ test/e2e/spec.js
@@ -1,16 +1,23 @@
 describe('todoapp homepage', function() {
   var todos = element.all(by.repeater('todo in todos'));
   
-  it('should have a title', function() {
+  beforeEach(function() {
     browser.get('http://localhost:3000');
+  })
 
+  it('should have a title', function() {
     expect(browser.getTitle()).toEqual('TodoApp');
   });
 
   it('should add new todo', function() {
-    browser.get('http://localhost:3000');
     element(by.model('newTodo')).sendKeys('todo test e2e');
+    element(by.model('priorityChoice')).click();
     element(by.id('addButton')).click();
-    expect(todos.count()).toEqual(3);
+    expect(todos.count()).toEqual(2);
+  })
+  it('should delete todo', function() {
+    element.all(by.css('.btn--delete')).last().click();
+    expect(todos.count()).toEqual(1);
+    
   })
 });
